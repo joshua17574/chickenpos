bd7a7760920670fc276130753cff3031aff9a53c
joshua17574 <57587240+joshua17574@users.noreply.github.com>
2026-06-16 17:26:42 +0800

    Delete chicken_cuts_pos directory

--- chicken_cuts_pos/android/app/src/main/kotlin/com/example/chicken_cuts_pos/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chicken_cuts_pos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- chicken_cuts_pos/server/src/vercel.js
+++
@@ -1,6 +0,0 @@
-import { initializeServer } from './server.js';
-
-export default async function handler(req, res) {
-  const { app } = await initializeServer();
-  return app(req, res);
-}
